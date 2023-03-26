778d4d4cda0feec615fc9a1def492b1df2c05558
Tanbir Ahmed <tanbir.ahmed02@outlook.com>
2023-03-26 16:18:29 -0400

    image filter with PILLOW Library

--- image-playground/images.py
+++ image-playground/images.py
@@ -1,4 +1,6 @@
-from PIL import Image
+from PIL import Image, ImageFilter
 
 img = Image.open('./Pokedex/199 pikachu.jpg')
-print(dir(img))
+filtered_img = img.convert('L')
+filtered_img.save("grey.png",'png')
+#png allows image filters
