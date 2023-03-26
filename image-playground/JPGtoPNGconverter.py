6dbfd282e8a60d99585a5935b4c029db1d01b021
Tanbir Ahmed <tanbir.ahmed02@outlook.com>
2023-03-26 18:12:07 -0400

    JPG to PNG converter DONE

---
+++ image-playground/JPGtoPNGconverter.py
@@ -0,0 +1,20 @@
+import sys
+import os
+from PIL import Image
+
+#grab first and second argument
+image_folder = sys.argv[1]
+output_folder = sys.argv[2]
+
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+for filename in os.listdir(image_folder):
+    img = Image.open(f'{image_folder}{filename}')
+    clean_name = os.path.splitext(filename)[0]
+    img.save(f'{output_folder}{clean_name}.png' ,'png')
+    print('all done')
+#check if new/ exists , if not create it
+#Loop thorugh pokedex and convert images to png
+#save them to the new folder
+
--- image-playground/images.py
+++ image-playground/images.py
@@ -1,6 +1,5 @@
 from PIL import Image, ImageFilter
 
-img = Image.open('./Pokedex/199 pikachu.jpg')
-filtered_img = img.convert('L')
-filtered_img.save("grey.png",'png')
-#png allows image filters
+img = Image.open('./201 astro.jpg')
+img.thumbnail((400, 400))
+img.save('thumbnail.jpg')
